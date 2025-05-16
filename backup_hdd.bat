40688bc1174228fcbf6409d9c839e292eddd005c
abuicke <buickea@gmail.com>
2025-05-16 18:25:00 +0100

    backup_hdd.bat

--- backup_hdd.bat
+++ backup_hdd.bat
@@ -3,7 +3,7 @@ echo Start time: %TIME%
 
 REM Make sure D:\ is my external hard drive by looking for the .HDD file in the root directory
 if not exist "D:\HDD" (
-    echo ERROR: Couldn't find .HDD file in D:\
+    echo ERROR: Couldn't find HDD file in D:\
     pause
     exit /b
 )
@@ -13,35 +13,84 @@ echo ===== gnupg =====
 rclone sync C:\Users\buick\AppData\Roaming\gnupg D:\gnupg --dry-run 2>&1 | gawk "!/NOTICE:/ || /Skipped (copy|delete)/" | sed -e "/Skipped copy/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/COPY: \1/" -e "/Skipped delete/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/DELETE: \1/"
 echo.
 
+echo Proceed with sync?
+pause
+
+rclone sync C:\Users\buick\AppData\Roaming\gnupg D:\gnupg D:\gnupg
+
+
+
 echo.
 echo ===== .gnupg =====
 rclone sync C:\Users\buick\.gnupg D:\.gnupg --dry-run 2>&1 | gawk "!/NOTICE:/ || /Skipped (copy|delete)/" | sed -e "/Skipped copy/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/COPY: \1/" -e "/Skipped delete/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/DELETE: \1/"
 echo.
+
+echo Proceed with sync?
+pause
+
+rclone sync C:\Users\buick\.gnupg D:\.gnupg
+
+
 
 echo.
 echo ===== bin =====
 rclone sync C:\Users\buick\bin D:\bin --dry-run 2>&1 | gawk "!/NOTICE:/ || /Skipped (copy|delete)/" | sed -e "/Skipped copy/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/COPY: \1/" -e "/Skipped delete/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/DELETE: \1/"
 echo.
 
+echo Proceed with sync?
+pause
+
+rclone sync C:\Users\buick\bin D:\bin
+
+
+
 echo.
 echo ===== Desktop =====
 rclone sync C:\Users\buick\Desktop D:\Desktop --dry-run 2>&1 | gawk "!/NOTICE:/ || /Skipped (copy|delete)/" | sed -e "/Skipped copy/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/COPY: \1/" -e "/Skipped delete/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/DELETE: \1/"
 echo.
+
+echo Proceed with sync?
+pause
+
+rclone sync C:\Users\buick\Desktop D:\Desktop
+
+
 
 echo.
 echo ===== Documents =====
 rclone sync C:\Users\buick\Documents D:\Documents --dry-run 2>&1 | gawk "!/NOTICE:/ || /Skipped (copy|delete)/" | sed -e "/Skipped copy/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/COPY: \1/" -e "/Skipped delete/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/DELETE: \1/"
 echo.
 
+echo Proceed with sync?
+pause
+
+rclone sync C:\Users\buick\Documents D:\Documents
+
+
+
 echo.
 echo ===== Music =====
 rclone sync C:\Users\buick\Music D:\Music --dry-run 2>&1 | gawk "!/NOTICE:/ || /Skipped (copy|delete)/" | sed -e "/Skipped copy/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/COPY: \1/" -e "/Skipped delete/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/DELETE: \1/"
 echo.
 
+echo Proceed with sync?
+pause
+
+rclone sync C:\Users\buick\Music D:\Music
+
+
+
 echo.
 echo ===== Pictures =====
 rclone sync C:\Users\buick\Pictures D:\Pictures --dry-run 2>&1 | gawk "!/NOTICE:/ || /Skipped (copy|delete)/" | sed -e "/Skipped copy/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/COPY: \1/" -e "/Skipped delete/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/DELETE: \1/"
 echo.
+
+echo Proceed with sync?
+pause
+
+rclone sync C:\Users\buick\Pictures D:\Pictures
+
+
 
 echo.
 echo ===== Videos =====
@@ -51,49 +100,10 @@ echo.
 echo Proceed with sync?
 pause
 
-echo.
-echo Syncing...
-echo.
-
-echo.
-echo ===== gnupg =====
-rclone sync C:\Users\buick\AppData\Roaming\gnupg D:\gnupg D:\gnupg
-echo.
-
-echo.
-echo ===== .gnupg =====
-rclone sync C:\Users\buick\.gnupg D:\.gnupg
-echo.
-
-echo.
-echo ===== bin =====
-rclone sync C:\Users\buick\bin D:\bin
-echo.
-
-echo.
-echo ===== Desktop =====
-rclone sync C:\Users\buick\Desktop D:\Desktop
-echo.
-
-echo.
-echo ===== Documents =====
-rclone sync C:\Users\buick\Documents D:\Documents
-echo.
-
-echo.
-echo ===== Music =====
-rclone sync C:\Users\buick\Music D:\Music
-echo.
-
-echo.
-echo ===== Pictures =====
-rclone sync C:\Users\buick\Pictures D:\Pictures
-echo.
-
-echo.
-echo ===== Videos =====
 rclone sync C:\Users\buick\Videos D:\Videos
-echo.
 
+
+
+echo Sync complete...
 echo End time: %TIME%
 pause

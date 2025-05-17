8d123d3a270d78a900011eba6ca54ecc25b61151
abuicke <buickea@gmail.com>
2025-05-18 00:29:51 +0100

    enabled proper warningm essages

--- backup_proton.bat
+++ backup_proton.bat
@@ -3,49 +3,49 @@ echo Start time: %TIME%
 
 echo.
 echo ===== gnupg =====
-rclone sync C:\Users\buick\AppData\Roaming\gnupg proton:\gnupg
+rclone sync C:\Users\buick\AppData\Roaming\gnupg proton:\gnupg -v
 
 echo.
 echo.
 echo.
 echo ===== .gnupg =====
-rclone sync C:\Users\buick\.gnupg proton:\.gnupg
+rclone sync C:\Users\buick\.gnupg proton:\.gnupg -v
 
 echo.
 echo.
 echo.
 echo ===== bin =====
-rclone sync C:\Users\buick\bin proton:\bin
+rclone sync C:\Users\buick\bin proton:\bin -v
 
 echo.
 echo.
 echo.
 echo ===== Desktop =====
-rclone sync C:\Users\buick\Desktop proton:\Desktop --exclude "desktop.ini" --exclude ".tmp.driveupload/**"
+rclone sync C:\Users\buick\Desktop proton:\Desktop -v --exclude "desktop.ini" --exclude ".tmp.driveupload/**"
 
 echo.
 echo.
 echo.
 echo ===== Documents =====
-rclone sync C:\Users\buick\Documents proton:\Documents --exclude "desktop.ini" --exclude "My Pictures" --exclude "My Music" --exclude "My Videos"
+rclone sync C:\Users\buick\Documents proton:\Documents -v --exclude "desktop.ini" --exclude "My Pictures" --exclude "My Music" --exclude "My Videos"
 
 echo.
 echo.
 echo.
 echo ===== Music =====
-rclone sync C:\Users\buick\Music proton:\Music --exclude "desktop.ini"
+rclone sync C:\Users\buick\Music proton:\Music -v --exclude "desktop.ini"
 
 echo.
 echo.
 echo.
 echo ===== Pictures =====
-rclone sync C:\Users\buick\Pictures proton:\Pictures --exclude "desktop.ini"
+rclone sync C:\Users\buick\Pictures proton:\Pictures -v --exclude "desktop.ini"
 
 echo.
 echo.
 echo.
 echo ===== Videos =====
-rclone sync C:\Users\buick\Videos proton:\Videos --exclude "desktop.ini"
+rclone sync C:\Users\buick\Videos proton:\Videos -v --exclude "desktop.ini"
 
 echo.
 echo.

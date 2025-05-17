8d543c8733935cd55d407f2721eefb57ef11b06c
abuicke <buickea@gmail.com>
2025-05-17 12:49:13 +0100

    backup_proton.bat

--- backup_proton.bat
+++ backup_proton.bat
@@ -3,112 +3,49 @@ echo Start time: %TIME%
 
 echo.
 echo ===== gnupg =====
-rclone sync C:\Users\buick\AppData\Roaming\gnupg proton:\gnupg --dry-run 2>&1 | gawk "!/NOTICE:/ || /Skipped (copy|delete)/" | sed -e "/Skipped copy/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/COPY: \1/" -e "/Skipped delete/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/DELETE: \1/"
-echo.
-
-echo Proceed with sync?
-pause
-
 rclone sync C:\Users\buick\AppData\Roaming\gnupg proton:\gnupg
-
-
 
 echo.
 echo.
 echo.
 echo ===== .gnupg =====
-rclone sync C:\Users\buick\.gnupg proton:\.gnupg --dry-run 2>&1 | gawk "!/NOTICE:/ || /Skipped (copy|delete)/" | sed -e "/Skipped copy/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/COPY: \1/" -e "/Skipped delete/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/DELETE: \1/"
-echo.
-
-echo Proceed with sync?
-pause
-
 rclone sync C:\Users\buick\.gnupg proton:\.gnupg
-
-
 
 echo.
 echo.
 echo.
 echo ===== bin =====
-rclone sync C:\Users\buick\bin proton:\bin --dry-run 2>&1 | gawk "!/NOTICE:/ || /Skipped (copy|delete)/" | sed -e "/Skipped copy/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/COPY: \1/" -e "/Skipped delete/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/DELETE: \1/"
-echo.
-
-echo Proceed with sync?
-pause
-
 rclone sync C:\Users\buick\bin proton:\bin
-
-
 
 echo.
 echo.
 echo.
 echo ===== Desktop =====
-rclone sync C:\Users\buick\Desktop proton:\Desktop --dry-run 2>&1 | gawk "!/NOTICE:/ || /Skipped (copy|delete)/" | sed -e "/Skipped copy/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/COPY: \1/" -e "/Skipped delete/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/DELETE: \1/"
-echo.
-
-echo Proceed with sync?
-pause
-
 rclone sync C:\Users\buick\Desktop proton:\Desktop
-
-
 
 echo.
 echo.
 echo.
 echo ===== Documents =====
-rclone sync C:\Users\buick\Documents proton:\Documents --dry-run --exclude "My Pictures" --exclude "My Music" --exclude "My Videos" 2>&1 | gawk "!/NOTICE:/ || /Skipped (copy|delete)/" | sed -e "/Skipped copy/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/COPY: \1/" -e "/Skipped delete/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/DELETE: \1/"
-echo.
-
-echo Proceed with sync?
-pause
-
 rclone sync C:\Users\buick\Documents proton:\Documents --exclude "My Pictures" --exclude "My Music" --exclude "My Videos"
-
-
 
 echo.
 echo.
 echo.
 echo ===== Music =====
-rclone sync C:\Users\buick\Music proton:\Music --dry-run 2>&1 | gawk "!/NOTICE:/ || /Skipped (copy|delete)/" | sed -e "/Skipped copy/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/COPY: \1/" -e "/Skipped delete/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/DELETE: \1/"
-echo.
-
-echo Proceed with sync?
-pause
-
 rclone sync C:\Users\buick\Music proton:\Music
-
-
 
 echo.
 echo.
 echo.
 echo ===== Pictures =====
-rclone sync C:\Users\buick\Pictures proton:\Pictures --dry-run 2>&1 | gawk "!/NOTICE:/ || /Skipped (copy|delete)/" | sed -e "/Skipped copy/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/COPY: \1/" -e "/Skipped delete/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/DELETE: \1/"
-echo.
-
-echo Proceed with sync?
-pause
-
 rclone sync C:\Users\buick\Pictures proton:\Pictures
-
-
 
 echo.
 echo.
 echo.
 echo ===== Videos =====
-rclone sync C:\Users\buick\Videos proton:\Videos --dry-run 2>&1 | gawk "!/NOTICE:/ || /Skipped (copy|delete)/" | sed -e "/Skipped copy/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/COPY: \1/" -e "/Skipped delete/ s/^[0-9\/:\ ]*NOTICE: \(.*\): Skipped.*/DELETE: \1/"
-echo.
-
-echo Proceed with sync?
-pause
-
 rclone sync C:\Users\buick\Videos proton:\Videos
-
 
 echo.
 echo.
